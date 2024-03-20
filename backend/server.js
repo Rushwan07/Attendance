fbb45b5053759cd8a156473494d9c19ce3c3d37a
Rushwan07 <rushdeveloper07@gmail.com>
2024-03-20 15:37:29 +0530

    Server updated

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,13 @@ app.use(express.json());
 // CORS middleware with specific origin
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://attendance-snowy-chi.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors(
+  {
+      origin: ["https://attendance-snowy-chi.vercel.app"],
+      methods: ["POST", "GET"],
+      credentials: true
+  }
+));
 
 // Routes
 app.use('/api/students', studentRoutes);
