7c6ab48d1cf1758fd893e9806d8c425b8c7f99f7
Rushwan07 <rushdeveloper07@gmail.com>
2024-03-20 14:17:09 +0530

    JJ

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const studentRoutes = require('./routes/studentRoutes')
-const userRoutes = require('./routes/user')
-require('dotenv').config()
+const cors = require('cors'); // Import the cors middleware
+const studentRoutes = require('./routes/studentRoutes');
+const userRoutes = require('./routes/user');
+require('dotenv').config();
 
 // Create an Express application
 const app = express();
@@ -10,18 +11,28 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+// CORS middleware with specific origin
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Routes
-app.use('/api/students', studentRoutes)
-app.use('/api/user', userRoutes)
+app.use('/api/students', studentRoutes);
+app.use('/api/user', userRoutes);
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT)
-    })
+      console.log('connected to db & listening on port', process.env.PORT);
+    });
   })
   .catch((error) => {
-    console.log(error)
-  })
+    console.log(error);
+  });
