ae67b7748fe32714004370dfbb841b7e669b9972
Rushwan07 <rushdeveloper07@gmail.com>
2024-03-20 16:24:27 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://attendance-snowy-chi.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin: *');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
