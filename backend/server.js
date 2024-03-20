78077e7cd8d002da9e00168120da1f118b9273bf
Rushwan07 <rushdeveloper07@gmail.com>
2024-03-20 16:01:10 +0530

    s

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(cors());
 
 app.use(cors(
   {
-      origin: [""],
+      origin: ["https://attendance-snowy-chi.vercel.app"],
       methods: ["POST", "GET", "DELETE"],
       credentials: true
   }
